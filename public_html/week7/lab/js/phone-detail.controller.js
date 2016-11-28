86528abec2fe1a825cef7ad59c6dd140e3d501fd
gregclavell-wm <gregclavell@gmail.com>
2016-11-28 15:49:32 -0500

    Added work up to step 3 to the lab in week7

---
+++ public_html/week7/lab/js/phone-detail.controller.js
@@ -0,0 +1,21 @@
+(function() {
+    
+    'use strict';
+    angular
+        .module('app')
+        .controller('PhoneDetailController', PhoneDetailController);
+
+    PhoneDetailController.$inject = ['$routeParams'];
+    
+    function PhoneDetailController($routeParams) {
+        var vm = this;
+        
+        vm.phone = {};
+        var id = $routeParams.phoneId;
+        
+        activate();
+        
+        function activate(){}
+    }
+     
+})();
